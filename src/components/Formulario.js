a21fb7ad419aa312704c753f5e6d5734cd138136
Freddy <freddy.artea@gmail.com>
2020-02-17 20:12:54 -0500

    documentando la App

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import Error from './Error'
+import PropTypes from 'prop-types';
+
 
 const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
 
@@ -83,4 +85,10 @@ const Formulario = ({busqueda, guardarBusqueda, guardarConsultar}) => {
      );
 }
  
+Formulario.propTypes = {
+    busqueda : PropTypes.object.isRequired,
+    guardarBusqueda : PropTypes.func.isRequired,
+    guardarConsultar : PropTypes.func.isRequired,
+}
+
 export default Formulario;
